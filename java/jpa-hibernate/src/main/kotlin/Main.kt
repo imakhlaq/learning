3ab9e852a7a51b0412274e842ef3fd15ecea0a47
imakhlaq <imakhlaqXD@gmail.com>
2024-12-28 03:13:32 +0530

    testing linting

--- java/jpa-hibernate/src/main/kotlin/Main.kt
+++ java/jpa-hibernate/src/main/kotlin/Main.kt
@@ -10,15 +10,12 @@ fun main() {
     p1.name = "Product name"
 
 
-
     println("test")
 
     println("Test2")
 
     println("id ${p1.id} name ${p1.name}")
 
-
-    
     println("id ${p1.id} name ${p1.name}")
 
 }
