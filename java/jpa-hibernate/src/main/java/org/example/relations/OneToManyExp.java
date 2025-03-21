ea0bccdfc198c71b5fe2e102591e2dbe6c1725ce
imakhlaq <imakhlaqXD@gmail.com>
2025-03-21 18:12:54 +0530

    one to many

--- java/jpa-hibernate/src/main/java/org/example/relations/OneToManyExp.java
+++ java/jpa-hibernate/src/main/java/org/example/relations/OneToManyExp.java
@@ -2,6 +2,9 @@ package org.example.relations;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class OneToManyExp {
 }
 
@@ -14,19 +17,57 @@ class classRoom {
     private String id;
     private String name;
 
+    @OneToMany
+    @JoinColumn(name = "classroom-id", referencedColumnName = "id")
+    private List<Student> students;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime.now();
+        //createdBy = LoggedUser.get();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        LocalDateTime.now();
+        //updatedBy = LoggedUser.get();
+    }
+
 }
 
 @Entity
 @Table
-class student {
+class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+}
+
+// bi-directional OneToMany
+@Entity
+@Table
+class classRoomBi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String idkk;
+    private String name;
+
+    @OneToMany(mappedBy = "classRoom", fetch = FetchType.EAGER)
+    private List<StudentBi> students;
+
+}
+
+@Entity
+@Table
+class StudentBi {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "classroom-id", referencedColumnName = "id")
-    private classRoom room;
-}
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "classroom-id", referencedColumnName = "idkk")
+    private classRoomBi classRoom;
 
-// bi-directional OneToMany
+}
